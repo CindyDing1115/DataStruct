ce075e8fe19eb007ea77e5b26e1dcc67c9414136
CindyDing1115 <1301312132@qq.com>
2018-03-11 22:05:05 +0800

    Update HeapSort.py

--- HeapSort.py
+++ HeapSort.py
@@ -15,7 +15,7 @@ def AdjustHeap(lists,i,size):
     temp = lists[i]
     j = 2 * i + 1
     while j < size:
-        if lists[j] < lists[j + 1]:
+        if lists[j] < lists[j + 1] and j+1 < size: # j+1 < size 比如进行到最后两项，size下没有右结点，则必然小于已排好序的 like 30
             j += 1
         if temp > lists[j]:
             break
@@ -24,6 +24,7 @@ def AdjustHeap(lists,i,size):
         j = j * 2 + 1
     lists[i] = temp
 
+
 def HeapSort(lists):
     """
     1 整体排序  2 微调
@@ -31,11 +32,17 @@ def HeapSort(lists):
     :return:
     """
     length = len(lists)
-    for i in range((length-1)/2)[::-1]:
+    #for i in range(0,(length-2)//2+1)[::-1]:
+    for i in range(0, length // 2)[::-1]:
         AdjustHeap(lists, i, length-1)
 
     for i in range(length)[::-1]:
         lists[i], lists[0] = lists[0], lists[i]
-        AdjustHeap(lists,0,i)
+        AdjustHeap(lists, 0, i)
 
+    return lists
+
+
+a = HeapSort([50, 10, 90, 30, 70, 40, 80, 60, 20])
+print(a)
 
