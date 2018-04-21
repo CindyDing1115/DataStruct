a26301c4d39614cf7af07ddef4c951f61dc51519
CindyDing1115 <1301312132@qq.com>
2018-04-22 00:27:21 +0800

    Update 6QuickSort.py

--- 6QuickSort.py
+++ 6QuickSort.py
@@ -11,18 +11,21 @@ def quick_sort(lists, left, right):
     key = lists[left]
     low = left
     high = right
+    
     while left < right:
-        while left < right and lists[right] >= key:
+        while lists[right]>=key and left < right:
             right -= 1
-        lists[left] = lists[right]
-        while left < right and lists[left] <= key:
+        lists[left] = lists[right] # 这里必须先查找right，因为key = lists[left];若先查找左边，会覆盖最后一个right值
+        
+        while lists[left] <= key and left < right:
             left += 1
-        lists[right] = lists[left]
-    lists[right] = key
-    quick_sort(lists, low, left-1)
-    quick_sort(lists, left+1, high)
+        lists[right] = lists[left] # 在这里覆盖正好还原了原始right位置应有的值
+        
+    lists[right] = key # 这里left == right 了
+    quick_sort(lists,low,left-1)
+    quick_sort(lists,left+1,high) # 这里的left已经找到了对应位置。
+    
     return lists
-
 
 a = quick_sort([50, 10, 90, 30, 70, 40, 80, 60, 20], 0, 8)
 print(a)
